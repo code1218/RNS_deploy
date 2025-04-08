4cd0da39cd546ebcce30ef8fa1e9176f6306cd7e
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-04-08 11:49:37 +0900

    imge

--- backend/src/main/java/com/korit/back/controller/TestController.java
+++ backend/src/main/java/com/korit/back/controller/TestController.java
@@ -3,6 +3,7 @@ package com.korit.back.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -17,5 +18,10 @@ public class TestController {
     @GetMapping("/oauth2/test")
     public ResponseEntity<String> test2() {
         return ResponseEntity.ok("oauth2");
+    }
+
+    @GetMapping("/image/{fileName}")
+    public ResponseEntity<String> test3(@PathVariable String fileName) {
+        return ResponseEntity.ok("image: " + fileName);
     }
 }
